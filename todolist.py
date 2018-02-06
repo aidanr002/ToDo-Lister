dcdc646dd031fdfc0ace139a852c240ee02fffe5
Aidan Ryan <aidanr002@gmail.com>
2018-02-06 18:12:36 +1000

    Added Move Function
    - Added a function to move items from any where in the list to any location desired.

--- todolist.py
+++ todolist.py
@@ -14,17 +14,17 @@ with open('todo.txt') as f: #Opens the files with a 'with' statement: closes whe
     user = input('Enter Command: ').lower()
 
     while user:
-        if user in 'list':
+        if user == 'list':
             for item in todo_list: #If called, prints the entire list
                 print (item)
 
-        if user in 'add':
-            new_item = input('New ToDo: ')
+        if user == 'add':
+            new_item = input('New ToDo: ').lower()
             if new_item != '':
                 todo_list.append(str(item_number) + ". " + new_item) #Adds a new item to the list with a number in front
                 item_number += 1
 
-        if user in 'remove':
+        if user == 'remove':
             remove_item = input('Enter ID to Remove: ') #Removes and item and adjusts all of the numbers
             if remove_item != '':
                 remove_item = int(remove_item)
@@ -39,11 +39,11 @@ with open('todo.txt') as f: #Opens the files with a 'with' statement: closes whe
                     item_number += 1
                 todo_list = todo_list_temp
 
-        if user in 'exit': #Quits the program
+        if user == 'exit': #Quits the program
             exit()
 
-        if user in 'search': #Search feature. Searches the entire list for the entered keyword. If found it prints the result with a new number based on the order it was in the original list.
-            search_query = input('Enter Search Query: ')
+        if user == 'search': #Search feature. Searches the entire list for the entered keyword. If found it prints the result with a new number based on the order it was in the original list.
+            search_query = input('Enter Search Query: ').lower()
             item_number = 1
             counter = 0
             for item in todo_list:
@@ -55,7 +55,7 @@ with open('todo.txt') as f: #Opens the files with a 'with' statement: closes whe
                         print(str(item_number) + '. ' + item[4:])
                     item_number += 1
 
-        if user in 'swap': #Swaps two items based on the inputed number
+        if user == 'swap': #Swaps two items based on the inputed number
             code_1 = input('Enter top item: ')
             code_2 = input('Enter bottom item: ')
             for item in todo_list:
@@ -79,20 +79,49 @@ with open('todo.txt') as f: #Opens the files with a 'with' statement: closes whe
                 item_number += 1
             todo_list = todo_list_temp
 
-        if user in 'clear': #Creates a clear function with Are You Sure to make sure you don't clear if you didn't want to.
+        if user == 'move':
+            counter = 0
+            selected_item = ''
+            selected_item_position = int(input("Select item to move: "))
+            for item in todo_list:
+                counter += 1
+                if counter ==  selected_item_position:
+                    if counter <= 9:
+                        selected_item = item[3:] #Changes the number of digits removed from the start based on the value (ten and above need more taken)
+                    if counter >= 10:
+                        selected_item = item[4:]
+            selected_item_newposition = int(input("Select new position: "))
+            counter = 0
+            item_number = 1
+            for item in todo_list:
+                counter += 1
+                if counter == selected_item_newposition:
+                    if counter <= 9:
+                        todo_list_temp.append(str(item_number) + '. ' + selected_item) #Changes the number of digits removed from the start based on the value (ten and above need more taken)
+                    if counter >= 10:
+                        todo_list_temp.append(str(item_number) + '. ' + selected_item)
+                if counter <= 9:
+                    todo_list_temp.append(str(item_number) + '. ' + item[3:]) #Changes the number of digits removed from the start based on the value (ten and above need more taken)
+                if counter >= 10:
+                    todo_list_temp.append(str(item_number) + '. ' + item[4:])
+                item_number += 1
+            todo_list = todo_list_temp
+
+        if user == 'clear': #Creates a clear function with Are You Sure to make sure you don't clear if you didn't want to.
             confirm = input('Are You Sure? ').lower()
             if 'yes' in confirm:
                 todo_list = []
                 print ('Cleared')
         todo_list_temp = []
-        if user in 'insertnew':
+
+        if user == 'insertnew':
             insert_where = int(input('Enter Location: '))
             counter = 0
             item_number = 1
             for item in todo_list:
                 counter += 1
                 if counter == insert_where:
-                    new_item = input("Enter Item: ")
+                    new_item = input("Enter Item: ").lower()
                     if counter <= 9:
                         todo_list_temp.append(str(item_number) + '. ' + new_item) #Changes the number of digits removed from the start based on the value (ten and above need more taken)
                     if counter >= 10:
