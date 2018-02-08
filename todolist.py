81630b88777df274e3a40d6af0e3999404b729d5
Aidan Ryan <aidanr002@gmail.com>
2018-02-08 13:19:54 +1000

    Bug Fix
    Stopped field non entry from crashing program

--- todolist.py
+++ todolist.py
@@ -44,16 +44,17 @@ with open('todo.txt') as f: #Opens the files with a 'with' statement: closes whe
 
         if user == 'search': #Search feature. Searches the entire list for the entered keyword. If found it prints the result with a new number based on the order it was in the original list.
             search_query = input('Enter Search Query: ').lower()
-            item_number = 1
-            counter = 0
-            for item in todo_list:
-                counter += 1
-                if search_query in item.lower():
-                    if counter <= 9:
-                        print(str(item_number) + '. ' + item[3:]) #Changes the number of digits removed from the start based on the value (ten and above need more taken)
-                    if counter >= 10:
-                        print(str(item_number) + '. ' + item[4:])
-                    item_number += 1
+            if search_query != '':
+                item_number = 1
+                counter = 0
+                for item in todo_list:
+                    counter += 1
+                    if search_query in item.lower():
+                        if counter <= 9:
+                            print(str(item_number) + '. ' + item[3:]) #Changes the number of digits removed from the start based on the value (ten and above need more taken)
+                        if counter >= 10:
+                            print(str(item_number) + '. ' + item[4:])
+                        item_number += 1
 
         if user == 'commands':
             print ('These are the commands: \n list, search, move, insertnew, exit, remove, add, clear')
@@ -62,55 +63,60 @@ with open('todo.txt') as f: #Opens the files with a 'with' statement: closes whe
             counter = 0
             selected_item = ''
             selected_item_position = int(input("Select item to move: "))
-            for item in todo_list:
-                counter += 1
-                if counter ==  selected_item_position:
-                    if counter <= 9:
-                        selected_item = item[3:] #Changes the number of digits removed from the start based on the value (ten and above need more taken)
-                    if counter >= 10:
-                        selected_item = item[4:]
-            selected_item_newposition = int(input("Select new position: "))
-            counter = 0
-            item_number = 1
-            for item in todo_list:
-                counter += 1
-                if counter == selected_item_newposition:
-                    if counter <= 9:
-                        todo_list_temp.append(str(item_number) + '. ' + selected_item) #Changes the number of digits removed from the start based on the value (ten and above need more taken)
-                    if counter >= 10:
-                        todo_list_temp.append(str(item_number) + '. ' + selected_item)
-                if counter <= 9:
-                    todo_list_temp.append(str(item_number) + '. ' + item[3:]) #Changes the number of digits removed from the start based on the value (ten and above need more taken)
-                if counter >= 10:
-                    todo_list_temp.append(str(item_number) + '. ' + item[4:])
-                item_number += 1
-            todo_list = todo_list_temp
+            if selected_item_position != '':
+                for item in todo_list:
+                    counter += 1
+                    if counter ==  selected_item_position:
+                        if counter <= 9:
+                            selected_item = item[3:] #Changes the number of digits removed from the start based on the value (ten and above need more taken)
+                        if counter >= 10:
+                            selected_item = item[4:]
+                selected_item_newposition = int(input("Select new position: "))
+                if selected_item_newposition != '':
+                    counter = 0
+                    item_number = 1
+                    for item in todo_list:
+                        counter += 1
+                        if counter == selected_item_newposition:
+                            if counter <= 9:
+                                todo_list_temp.append(str(item_number) + '. ' + selected_item) #Changes the number of digits removed from the start based on the value (ten and above need more taken)
+                            if counter >= 10:
+                                todo_list_temp.append(str(item_number) + '. ' + selected_item)
+                        if counter <= 9:
+                            todo_list_temp.append(str(item_number) + '. ' + item[3:]) #Changes the number of digits removed from the start based on the value (ten and above need more taken)
+                        if counter >= 10:
+                            todo_list_temp.append(str(item_number) + '. ' + item[4:])
+                        item_number += 1
+                    todo_list = todo_list_temp
 
         if user == 'clear': #Creates a clear function with Are You Sure to make sure you don't clear if you didn't want to.
             confirm = input('Are You Sure? ').lower()
-            if 'yes' in confirm:
-                todo_list = []
-                print ('Cleared')
+            if confirm != '':
+                if 'yes' in confirm:
+                    todo_list = []
+                    print ('Cleared')
         todo_list_temp = []
 
         if user == 'insertnew':
             insert_where = int(input('Enter Location: '))
-            counter = 0
-            item_number = 1
-            for item in todo_list:
-                counter += 1
-                if counter == insert_where:
-                    new_item = input("Enter Item: ").lower()
+            if insert_where != '':
+                counter = 0
+                item_number = 1
+                for item in todo_list:
+                    counter += 1
+                    if counter == insert_where:
+                        new_item = input("Enter Item: ").lower()
+                        if new_item != '':
+                            if counter <= 9:
+                                todo_list_temp.append(str(item_number) + '. ' + new_item) #Changes the number of digits removed from the start based on the value (ten and above need more taken)
+                            if counter >= 10:
+                                todo_list_temp.append(str(item_number) + '. ' + new_item)
                     if counter <= 9:
-                        todo_list_temp.append(str(item_number) + '. ' + new_item) #Changes the number of digits removed from the start based on the value (ten and above need more taken)
+                        todo_list_temp.append(str(item_number) + '. ' + item[3:]) #Changes the number of digits removed from the start based on the value (ten and above need more taken)
                     if counter >= 10:
-                        todo_list_temp.append(str(item_number) + '. ' + new_item)
-                if counter <= 9:
-                    todo_list_temp.append(str(item_number) + '. ' + item[3:]) #Changes the number of digits removed from the start based on the value (ten and above need more taken)
-                if counter >= 10:
-                    todo_list_temp.append(str(item_number) + '. ' + item[4:])
-                item_number += 1
-            todo_list = todo_list_temp
+                        todo_list_temp.append(str(item_number) + '. ' + item[4:])
+                    item_number += 1
+                todo_list = todo_list_temp
 
         n = open('todo.txt', 'w') #Opens the file to write- will over ride all other information in the file.
         item_number = 1
